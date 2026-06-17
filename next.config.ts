73c923d6f34da9ce4df611453baa15bc2eabbd54
luis_aquije <132310126+LuisAquije123@users.noreply.github.com>
2026-06-17 18:25:08 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   allowedDevOrigins: ['192.168.0.103', 'db27-181-65-0-35.ngrok-free.app', '*.ngrok-free.app', '*.ngrok.io'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
+
+
 
 export default nextConfig;
